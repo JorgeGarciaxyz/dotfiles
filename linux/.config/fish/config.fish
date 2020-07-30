3d0d82a0f7de49ec7bf2ffab127896b0e955075c
Jorge Garcia <jorge.mgg94@gmail.com>
2020-07-29 19:33:34 -0500

    Add more fish functions

--- linux/.config/fish/config.fish
+++ linux/.config/fish/config.fish
@@ -1,1 +1,5 @@
 function gss; git status; end
+function dockercompose; docker-compose up -d; end
+function be; bundle exec; end
+function rc; bundle exec rails c; end
+function rs; bundle exec rails s; end
