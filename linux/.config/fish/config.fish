3d1683a32fb420f0b144aac6820138287aaca57c
Jorge Garcia <jorge.mgg94@gmail.com>
2020-08-21 22:57:47 -0500

    Fix Tmux colors with vim

--- linux/.config/fish/config.fish
+++ linux/.config/fish/config.fish
@@ -3,3 +3,4 @@ function dockercompose; docker-compose up -d; end
 function be; bundle exec; end
 function rc; bundle exec rails c; end
 function rs; bundle exec rails s; end
+alias tmux="TERM=screen-256color-bce command tmux"
