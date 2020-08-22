462b3b662eb61d4b75b3db605df358703bbb3ed7
Jorge Garcia <jorge.mgg94@gmail.com>
2020-08-22 15:01:49 -0500

    Auto start tmux on fish

--- linux/.config/fish/config.fish
+++ linux/.config/fish/config.fish
@@ -4,3 +4,10 @@ function be; bundle exec; end
 function rc; bundle exec rails c; end
 function rs; bundle exec rails s; end
 alias tmux="TERM=screen-256color-bce command tmux"
+
+# Link: https://medium.com/@HazuliFidastian/run-tmux-automatically-on-fish-shell-2b62622661c7
+if not set -q TMUX
+    set -g TMUX tmux new-session -d -s base
+    eval $TMUX
+    tmux attach-session -d -t base
+end
